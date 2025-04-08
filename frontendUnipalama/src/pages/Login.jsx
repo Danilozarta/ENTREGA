5c89638831fd69e7b2c760a68a53a5ae62d3c583
Brayan Gonzalez <b.sleider15@gmail.com>
2025-04-07 22:44:21 -0500

    Solucionado el problema de campos llenos despues del envio del formulario, agregadas funcionalidades de nuevo registro o volver al inicio en la alerta

--- frontendUnipalama/src/pages/Login.jsx
+++ frontendUnipalama/src/pages/Login.jsx
@@ -130,8 +130,7 @@ const Login = () => {
                             <button
                                 type="submit"
                                 disabled={cargando}
-                                className={`boton-entrar-epp group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${cargando ? 'opacity-50 cursor-not-allowed' : ''}`}
-                            >
+                                className={`w-full py-3 px-4 text-white bg-indigo-600 hover:bg-indigo-700 rounded-md font-medium text-base sm:text-sm transition-colors duration-200 ${cargando ? 'opacity-50 cursor-not-allowed' : ''}`}>
                                 {cargando ? 'Iniciando sesión...' : 'Iniciar sesión'}
                             </button>
                         </div>
--- frontendUnipalama/src/pages/productos/RegistroTrabajador.jsx
+++ frontendUnipalama/src/pages/productos/RegistroTrabajador.jsx
@@ -1,4 +1,4 @@
-import { Navigate } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import Swal from "sweetalert2";
 
@@ -8,10 +8,11 @@ import Navbar from "../../components/Navbar";
 import Alerta from "../../components/Alerta";
 import React, { useState } from 'react';
 
-import { Link } from 'react-router-dom'; // Para manejar la navegación
 
 
 const RegistroTrabajador = () => {
+    const navigate = useNavigate(); 
+
     const [nombre, setNombre] = useState('');
     const [apellido, setApellido] = useState('');
     const [cedula, setCedula] = useState('');
@@ -19,6 +20,10 @@ const RegistroTrabajador = () => {
     const [centro_de_operacion, setcentro_de_operacion] = useState('');
     const [empresa, setEmpresa] = useState('');
     const [contacto, setContacto] = useState('');
+
+    // Genera un prefijo único para todos los campos en esta instancia
+    const [formId] = useState(() => `form_${Math.random().toString(36).substr(2, 5)}`);
+   
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -34,7 +39,7 @@ const RegistroTrabajador = () => {
         };
 
         console.log('Datos enviados:', trabajador);
-
+        
         try {
             const response = await fetch('http://localhost:4000/api/epp/registrar-trabajador', {
                 method: 'POST',
@@ -48,7 +53,33 @@ const RegistroTrabajador = () => {
 
             const data = await response.json();
             if (data.success) {
-                Swal.fire('Éxito', 'Trabajador registrado correctamente', 'success');
+                    // Limpiar los campos antes de que el usuario haga clic en  las opciones de alerta "Aceptar"
+                    setNombre('');
+                    setApellido('');
+                    setCedula('');
+                    setCargo('');
+                    setcentro_de_operacion('');
+                    setEmpresa('');
+                    setContacto('');
+
+                    // Alerta con botones
+                    Swal.fire({
+                        title: '¡Éxito!',
+                        text: 'Trabajador registrado correctamente',
+                        icon: 'success',
+                        confirmButtonText: 'Nuevo registro',
+                        cancelButtonText: 'Ir al inicio',
+                        showCancelButton: true,
+                        confirmButtonColor: '#1abc9c',
+                        cancelButtonColor: '#bdc3c7',
+                        allowOutsideClick: false,
+                    }).then((result) => {
+                        if (result.isDismissed ) {
+                            navigate("/homeHs"); // Redirige a /homeHs
+                        }
+                        // Si hace clic en "Nuevo registro", no se hace nada (el formulario ya está limpio)
+                    });
+        
             } else {
                 Swal.fire('Error', 'No se pudo registrar el trabajador', 'error');
             }
@@ -59,124 +90,130 @@ const RegistroTrabajador = () => {
     };
 
     return (
-        <div className="body-registro">
-            {/* Menú de navegación */}
-            <nav className="nav-registro">
-                <div className="hamburger-registro" id="hamburger-registro">
-                    <div className="line line1"></div>
-                    <div className="line line2"></div>
+        <div className="registro-container">
+            <h2>Registro de Trabajador</h2>
+            <form 
+                onSubmit={handleSubmit} 
+                autoComplete="off"  // Desactiva autocompletado en el formulario
+                className="registro-form"        // ID único para el formulario
+            >
+                {/* Campo Nombre */}
+                <div className="form-group">
+                    <label htmlFor="nombre">Nombre:</label>
+                    <input
+                        type="text"
+                        id="nombre"
+                        className="registro-input"
+                        value={nombre}
+                        onChange={(e) => setNombre(e.target.value)}
+                        name={`${formId}_nombre`}  // Nombre único
+                        autoComplete="new-password" // Truco infalible
+                        required
+                    />
                 </div>
-                <ul id="menu" className="menu">
-                    <li>
-                        <Link to="/home" className="nav-link-registro">Inicio</Link>
-                    </li>
-                    <li>
-                        <Link to="/" className="nav-link-registro">Cerrar Sesión</Link>
-                    </li>
-                </ul>
-            </nav>
 
-            {/* Contenedor del formulario */}
-            <div className="registro-container">
-                <h2>Registro de Trabajador</h2>
-                <form id="registroForm" className="registro-form" onSubmit={handleSubmit}>
-                    <div className="form-group">
-                        <label htmlFor="nombre" className="registro-label">
-                            Nombre:
-                        </label>
-                        <input
-                            type="text"
-                            id="nombre"
-                            className="registro-input"
-                            value={nombre}
-                            onChange={(e) => setNombre(e.target.value)}
-                            required
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="apellido" className="registro-label">
-                            Apellido:
-                        </label>
-                        <input
-                            type="text"
-                            id="apellido"
-                            className="registro-input"
-                            value={apellido}
-                            onChange={(e) => setApellido(e.target.value)}
-                            required
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="cedula" className="registro-label">
-                            Cédula:
-                        </label>
-                        <input
-                            type="text"
-                            id="cedula"
-                            className="registro-input"
-                            value={cedula}
-                            onChange={(e) => setCedula(e.target.value)}
-                            required
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="cargo" className="registro-label">
-                            Cargo:
-                        </label>
-                        <input
-                            id="cargo"
-                            className="registro-input"
-                            type="text"
-                            value={cargo}
-                            onChange={(e) => setCargo(e.target.value)}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="centro_de_operacion" className="registro-label">
-                            Centro de operacion:
-                        </label>
-                        <input
-                            type="text"
-                            id="centro_de_operacion"
-                            value={centro_de_operacion}
-                            onChange={(e) => setcentro_de_operacion(e.target.value)}
-                            className="registro-input"
-                            required
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="empresa" className="registro-label">
-                            Empresa:
-                        </label>
-                        <input
-                            type="text"
-                            id="empresa"
-                            value={empresa}
-                            onChange={(e) => setEmpresa(e.target.value)}
-                            name="empresa"
-                            className="registro-input"
-                            required
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="contacto" className="registro-label">
-                            Número de Contacto:
-                        </label>
-                        <input
-                            type="text"
-                            id="contacto"
-                            value={contacto}
-                            onChange={(e) => setContacto(e.target.value) }
-                            name="contacto"
-                            className="registro-input"
-                            required
-                        />
-                    </div>
-                    <button type="submit" className="registro-button">
-                        Registrar
-                    </button>
-                </form>
-            </div>
+                {/* Campo Apellido */}
+                <div className="form-group">
+                    <label htmlFor="apellido">Apellido:</label>
+                    <input
+                        type="text"
+                        id="apellido"
+                        className="registro-input"
+                        value={apellido}
+                        onChange={(e) => setApellido(e.target.value)}
+                        name={`${formId}_apellido`}
+                        autoComplete="new-password"
+                        required
+                    />
+                </div>
+
+                {/* Campo Cédula */}
+                <div className="form-group">
+                    <label htmlFor="cedula">Cédula:</label>
+                    <input
+                        type="number"
+                        id="cedula"
+                        className="registro-input"
+                        value={cedula}
+                        onChange={(e) => setCedula(e.target.value.replace(/\D/g, ''))}
+                        name={`${formId}_cedula`}
+                        autoComplete="new-password"
+                        required
+                        style={{ 
+                            WebkitAppearance: 'none',
+                            MozAppearance: 'textfield',
+                        }}
+                    />
+                </div>
+
+                {/* Campo Cargo */}
+                <div className="form-group">
+                    <label htmlFor="cargo">Cargo:</label>
+                    <input
+                        type="text"
+                        id="cargo"
+                        className="registro-input"
+                        value={cargo}
+                        onChange={(e) => setCargo(e.target.value)}
+                        name={`${formId}_cargo`}
+                        autoComplete="new-password"
+                        required
+                    />
+                </div>
+
+                {/* Campo Centro de Operación */}
+                <div className="form-group">
+                    <label htmlFor="centro_de_operacion">Centro de Operación:</label>
+                    <input
+                        type="text"
+                        id="centro_de_operacion"
+                        className="registro-input"
+                        value={centro_de_operacion}
+                        onChange={(e) => setcentro_de_operacion(e.target.value)}
+                        name={`${formId}_centro_operacion`}
+                        autoComplete="new-password"
+                        required
+                    />
+                </div>
+
+                {/* Campo Empresa */}
+                <div className="form-group">
+                    <label htmlFor="empresa">Empresa:</label>
+                    <input
+                        type="text"
+                        id="empresa"
+                        className="registro-input"
+                        value={empresa}
+                        onChange={(e) => setEmpresa(e.target.value)}
+                        name={`${formId}_empresa`}
+                        autoComplete="new-password"
+                        required
+                    />
+                </div>
+
+                {/* Campo Contacto */}
+                <div className="form-group">
+                    <label htmlFor="contacto">Teléfono:</label>
+                    <input
+                        type="number"
+                        id="contacto"
+                        className="registro-input"
+                        value={contacto}
+                        onChange={(e) => setContacto(e.target.value.replace(/\D/g, ''))}
+                        name={`${formId}_contacto`}
+                        autoComplete="new-password"
+                        required
+                        style={{ 
+                            WebkitAppearance: 'none',
+                            MozAppearance: 'textfield',
+                        }}
+                    />
+                </div>
+
+                <button type="submit" className="registro-button">
+                    Registrar
+                </button>
+            </form>
         </div>
     );
 };
